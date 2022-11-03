61da6ca106633570e1f7e90fdb3b6a09620a0a70
Akshat Bhuryan <me@here.com>
2022-11-03 09:39:42 +0530

    Prime code fixed by AK

--- prime_problem.js
+++ prime_problem.js
@@ -1,14 +1,20 @@
+function check_prime(num){
+
 var count=0;
 
 for(var i=1; i<=num; i++){
-if(num%2==0){
-count--
+if(num%i==0){
+count++;
 }
 }
 
 if(count==2){
-console.log("Prime");
+console.log("Prime Number");
 }
 else{
-console.log("No");
+console.log("Not a Prime Number");
 }
+
+}
+
+check_prime(13);
